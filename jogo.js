39b3dce8af1ed3f0e85b6a5b696e26b7bf4e4282
MaruzamB <maruzambueno@gmail.com>
2024-09-25 21:38:01 -0300

    Criado posições Randomicas

--- jogo.js
+++ jogo.js
@@ -12,3 +12,22 @@ function ajustaTamanhoTelaJogo(){
 
 ajustaTamanhoTelaJogo()
 
+//logica para posiçoes randomicas
+
+var posicaoX = Math.floor(Math.random() * largura) - 100;
+var posicaoY = Math.floor(Math.random()* altura) - 100;
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX;
+posicaoY = posicaoY < 0 ? 0 : posicaoY;
+console.log(posicaoX, posicaoY);
+
+//criar o elemento html
+
+var mosca = document.createElement('img');
+mosca.src = 'imagens/mosca.png';
+mosca.className = 'mosquito1'
+mosca.style.left = posicaoX + 'px'
+mosca.style.top = posicaoY + 'px'
+mosca.style.position = 'absolute'
+
+document.body.appendChild(mosca);
