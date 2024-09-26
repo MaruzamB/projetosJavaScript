014c1b77cbfe536119217b260dc7b68620f323ed
MaruzamB <maruzambueno@gmail.com>
2024-09-26 19:04:51 -0300

    criado cronometro

--- jogo.js
+++ jogo.js
@@ -3,6 +3,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
 
 function ajustaTamanhoTelaJogo(){
      altura = window.innerHeight
@@ -12,6 +13,20 @@ function ajustaTamanhoTelaJogo(){
 }
 
 ajustaTamanhoTelaJogo()
+
+var cronometro = setInterval(function(){
+     
+     tempo -= 1;
+     if(tempo < 0){
+          clearInterval(cronometro)
+          clearInterval(criarMosca)
+          alert('vitoria')
+     }else{
+          document.getElementById('cronometro').innerHTML = tempo;
+     }
+    
+     
+},1000);
 
 //logica para posiçoes randomicas
 
